09c351aa856e3f1591f3d916bbd4b1cf697516c4
cskrocki17 <32140925+cskrocki17@users.noreply.github.com>
2017-10-02 11:04:11 -0400

    Add files via upload

--- madlibs.js
+++ madlibs.js
@@ -1,22 +1,42 @@
 //Cherylann Skrocki
 //Mad Libs
 
+//Initialize variables
 var sentence = [];
 var replacements = [];
 var index = 0;
 
+//Initialize users
 alert("filler go away");
-alert("End with shift 6");
-populate(index,sentence);
-alert (populate(index,sentence));
+alert("Enter after each word, end with shift 6");
 
-function populate(sindex,sarray){
+//fill sentence
+populate(index,sentence,"word");
+alert (sentence);
+index = 0;
+//specifiy positions
+populate(index,replacements,"position");
+alert (replacements);
+index = 0
+replace(index,sentence,replacements,"part of speech")
+alert(sentence)
+
+function populate(sindex,sarray,label){
 	
 	do{
-		sarray[sindex] = prompt("Next Word?");
+		sarray[sindex]=prompt("Next " + label + "?");
 		sindex = sindex+1;
 	}
 	while(sarray[sindex-1] != "^");
 	console.log(sarray);
+	sarray.splice(-1,1);
+	return sarray;
+}
+
+function replace(sindex,sarray,rarray,label){
+	
+	for (sindex = 0; sindex < rarray.length; sindex++){
+		sarray[rarray[sindex]] = prompt ("Next " + label + "?");
+	}
 	return sarray;
 }
