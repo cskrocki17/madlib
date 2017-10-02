153caf89533ad8c84a184e4fb51035be97dffb70
cskrocki17 <32140925+cskrocki17@users.noreply.github.com>
2017-10-02 14:00:32 -0400

    Add files via upload

--- madlibs.js
+++ madlibs.js
@@ -17,9 +17,18 @@ index = 0;
 //specifiy positions
 populate(index,replacements,"position");
 alert (replacements);
-index = 0
-replace(index,sentence,replacements,"part of speech")
-alert(sentence)
+index = 0;
+replace(index,sentence,replacements,"part of speech");
+alert(sentence);
+//switch people
+//train filler
+alert("Creator leave, filler return. Enter words that match the parts of speech displayed");
+index = 0;
+//show sentence
+replace(index,sentence,replacements,"replacement");
+//show sentence with spaces instead of commas
+sentenceString = sentence.join(' ');
+alert(sentenceString.toString());
 
 function populate(sindex,sarray,label){
 	
@@ -36,7 +45,12 @@ function populate(sindex,sarray,label){
 function replace(sindex,sarray,rarray,label){
 	
 	for (sindex = 0; sindex < rarray.length; sindex++){
-		sarray[rarray[sindex]] = prompt ("Next " + label + "?");
+		if (label == "replacement"){
+			sarray[rarray[sindex]] =  prompt("Provide a "+sarray[rarray[sindex]]+ " here: ");
+		}
+		else{
+			sarray[rarray[sindex]] = prompt ("Next " + label + "?");			
+		}
 	}
 	return sarray;
 }
